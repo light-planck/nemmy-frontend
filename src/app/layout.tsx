73e5700b4ae8c53b7705f61141514e5437c381e6
light-planck <seseifunc16@gmail.com>
2023-11-18 02:45:57 +0900

    feat: dark mode

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,6 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
 import { Providers } from "./providers";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "nemmy",
@@ -16,8 +13,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>
+    <html lang="ja" className="dark">
+      <body>
         <Providers>{children}</Providers>
       </body>
     </html>
