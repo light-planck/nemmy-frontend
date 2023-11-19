416d4ca2544e9a547687332219464272d3183edd
light-planck <seseifunc16@gmail.com>
2023-11-19 22:25:44 +0900

    feat: swrで利用するfetcher

--- src/lib/fetcher.ts
+++ src/lib/fetcher.ts
@@ -1,1 +1,14 @@
-export const fetcher = (url: string) => fetch(url).then((r) => r.json());
+export const fetcher = (url: string) =>
+  fetch(url)
+    .then((res) => {
+      if (!res.ok) {
+        throw new Error("Network response was not ok");
+      }
+      return res.json();
+    })
+    .catch((error) => {
+      console.error(
+        "There has been a problem with your fetch operation:",
+        error,
+      );
+    });
