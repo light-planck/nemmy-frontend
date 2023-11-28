2ef4b92a7eccca6ee82f15313aba5d7a72dbcd8f
light-planck <seseifunc16@gmail.com>
2023-11-29 00:29:12 +0900

    refactor: モジュール名を変更

--- src/app/search/users/hooks/useUser.ts
+++ src/app/search/users/hooks/useUser.ts
@@ -2,14 +2,14 @@ import { useState } from "react";
 import useSWRMutation from "swr/mutation";
 
 import { fetcher } from "@/lib/fetcher";
-import { getBaseAPIEndPoint } from "@/lib/getBaseAPIEndPoint";
+import { getBaseURL } from "@/lib/getBaseURL";
 
 import { User } from "../types";
 
 export const useUser = () => {
   const [username, setUsername] = useState("");
   const { data, isMutating, trigger } = useSWRMutation<User>(
-    getBaseAPIEndPoint() + `/users/${username}`,
+    getBaseURL() + `/users/${username}`,
     fetcher,
   );
 
